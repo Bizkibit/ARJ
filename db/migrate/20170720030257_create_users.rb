08350d654c207164b7e0cbe31c577cb3b2076985
Farshid HM <fhmanesh@gmail.com>
2017-07-19 20:31:03 -0700

    create user model

---
+++ db/migrate/20170720030257_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def up
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.date :dob
+      t.string :username
+      t.string :picture
+      t.text :bio
+      t.boolean :is_admin
+      t.string :password_digest
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+  end
+
+  def down
+    drop_table :users
+  end
+end
