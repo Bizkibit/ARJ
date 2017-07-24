a06afabbb49acdc78919d06b7e880494ebcc00af
Farshid HM <fhmanesh@gmail.com>
2017-07-24 10:16:37 -0700

    fixed seeds and organization panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Attendance.destroy_all
 Event.destroy_all
 Application.destroy_all
 Organization.destroy_all
